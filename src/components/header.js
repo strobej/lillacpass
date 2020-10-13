019dc349f117957b42977249584bb339a8158f7a
Selma <selma.delic@outlook.com>
2020-10-13 18:37:44 +0200

    added header

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header() {
+    return <div>
+    </div>
+} 
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from '../src/components/header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
