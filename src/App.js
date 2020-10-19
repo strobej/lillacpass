6229a457e78a86902fa4b18e98d096a0eb2c4290
Selma <selma.delic@outlook.com>
2020-10-19 19:51:07 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Footer from '../src/components/Footer';
 import Header from '../src/components/Header';
 
 function App() {
-  return <div>
-         <Header />
-         <Footer /> 
-  </div>
+  return (
+  <div>
+    <Header />
+    <Footer /> 
+  </div>)
 }
 export default App;
